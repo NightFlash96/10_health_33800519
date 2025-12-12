86e81f0fbaaabc8fc6d6f81542bc89cfb85ba69e
NightFlash96 <149069326+NightFlash96@users.noreply.github.com>
2025-12-12 00:50:36 +0000

    session containers added sorted by date

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -8,8 +8,12 @@ INSERT INTO users (id, username, email, password_hash, created) VALUES
 (0, "gold", "gold@smiths.com", "", NOW());
     
 # Insert test climbs
-INSERT INTO climbs (id, user_id, climb_name, difficulty, date_climbed) VALUES
-(0, 1, "Cliff Edge", "V2", NOW());
+INSERT INTO climbs (user_id, climb_name, difficulty, date_climbed) VALUES
+(1, "Hard Hang", "V2", "2025-12-11 23:25:30");
+INSERT INTO climbs (user_id, climb_name, difficulty, date_climbed) VALUES
+(1, "Cliff Edge", "V4", "2025-12-11 23:44:33");
+INSERT INTO climbs (user_id, climb_name, difficulty, date_climbed) VALUES
+(1, "Midnight Climb", "V3", "2025-12-12 00:02:51");
 
 # Insert test posts
 INSERT INTO posts (id, user_id, title, content, created, parentpost) VALUES
